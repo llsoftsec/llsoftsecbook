4e4f237872e02d6990ae89909969d8c8d91ea658
kbeyls <kbeyls@users.noreply.github.com>
2021-10-04 14:46:39 +0000

    deploy: 4e17238a4aa975872bdf1a30f9d2aee62f2b24da

--- build_with_docker.sh
+++
@@ -1,1 +0,0 @@
-docker build -t llsoftsecbook_build docker && docker run --rm --mount type=bind,source="$(pwd)",target=/src llsoftsecbook_build all
--- docker/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cd /src
-echo $PWD
-ls -al
-sh -c "make $*"
