814c89f7697855918c318c0c9734b1c94f0acccf
Jonathan Louis Kaplan <Louis.Kaplan@arm.com>
2022-02-23 23:22:39 +0000

    Revert "Make build output belong to user who starts build"
    This reverts commit 4ac493aff055e29eb949f6535781d2043ea70f70.

--- build_with_docker.sh
+++ build_with_docker.sh
@@ -1,3 +1,1 @@
-build_args="--build-arg uname=${USER} --build-arg uid=$(id -u)"
-docker build $build_args -t llsoftsecbook_build docker
-docker run --rm --mount type=bind,source="$(pwd)",target=/src llsoftsecbook_build all
+docker build -t llsoftsecbook_build docker && docker run --rm --mount type=bind,source="$(pwd)",target=/src llsoftsecbook_build all
