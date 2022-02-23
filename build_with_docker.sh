b16ab8eb03281f9b2bb0db0910643597f9eedcc0
Jonathan Louis Kaplan <Louis.Kaplan@arm.com>
2022-02-23 23:02:10 +0000

    Make build output belong to user who starts build
    * Ensures that build output using
      the docker build script belongs
      to the user and group who own the
      build script itself.
    
    * Achieves this through use of `stat`
      linux utility and the `--user` flag
      of `docker run`.

--- build_with_docker.sh
+++ build_with_docker.sh
@@ -1,1 +1,4 @@
-docker build -t llsoftsecbook_build docker && docker run --rm --mount type=bind,source="$(pwd)",target=/src llsoftsecbook_build all
+readonly base="$(dirname "${BASH_SOURCE[0]}")"
+readonly uid="$(stat -c "%u" $base)"
+readonly gid="$(stat -c "%g" $base)"
+docker build -t llsoftsecbook_build docker && docker run --rm --user=${uid}:${gid} --mount type=bind,source="$(pwd)",target=/src llsoftsecbook_build all
