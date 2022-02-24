5b3cb0a26b7f18b0ef9c611dab8729c9ca3ca6c6
Jonathan Louis Kaplan <Louis.Kaplan@arm.com>
2022-02-24 22:10:29 +0000

    Make build output match owners of build script
    * Ensures that build output using
      the docker build script belongs
      to the user and group who own the
      build script itself.

--- build_with_docker.sh
+++ build_with_docker.sh
@@ -1,4 +1,5 @@
-readonly script_path="${BASH_SOURCE[0]}"
-readonly uid="$(stat -c "%u" $script_path)"
-readonly gid="$(stat -c "%g" $script_path)"
-docker build -t llsoftsecbook_build docker && docker run --rm --user=${uid}:${gid} --mount type=bind,source="$(pwd)",target=/src llsoftsecbook_build all
+#!/bin/sh
+readonly script_path="$0"
+readonly uid="$(stat -c "%u" "${script_path}")"
+readonly gid="$(stat -c "%g" "${script_path}")"
+docker build -t llsoftsecbook_build docker && docker run --rm --user="${uid}":"${gid}" --mount type=bind,source="$(pwd)",target=/src llsoftsecbook_build all
