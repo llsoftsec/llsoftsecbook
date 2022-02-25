9876604e0b27969d6bc69fa59613ee2ce77075e7
Jonathan Louis Kaplan <90251161+JLouisKaplan-Arm@users.noreply.github.com>
2022-02-25 09:23:02 +0000

    Make build output belong to user who starts build (#86)
    * Ensures that build output using
      the Docker build script belongs
      to the user who triggered the build.
    
    * Tested for OSX/Linux cross-platform
      compatibility and Posix shell compliance.

--- build_with_docker.sh
+++ build_with_docker.sh
@@ -1,1 +1,4 @@
-docker build -t llsoftsecbook_build docker && docker run --rm --mount type=bind,source="$(pwd)",target=/src llsoftsecbook_build all
+#!/bin/sh
+readonly uid="$(id -u)"
+readonly gid="$(id -g)"
+docker build -t llsoftsecbook_build docker && docker run --rm --user="${uid}":"${gid}" --mount type=bind,source="$(pwd)",target=/src llsoftsecbook_build all
