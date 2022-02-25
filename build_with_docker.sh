6cf065bd3e1b7ac055cc37ba86b04c43d604f481
Jonathan Louis Kaplan <Louis.Kaplan@arm.com>
2022-02-25 09:08:43 +0000

    Change `stat` to `id`
    * for OSX/Linux cross-platform compatibility

--- build_with_docker.sh
+++ build_with_docker.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-readonly script_path="$0"
-readonly uid="$(stat -c "%u" "${script_path}")"
-readonly gid="$(stat -c "%g" "${script_path}")"
+readonly uid="$(id -u)"
+readonly gid="$(id -g)"
 docker build -t llsoftsecbook_build docker && docker run --rm --user="${uid}":"${gid}" --mount type=bind,source="$(pwd)",target=/src llsoftsecbook_build all
