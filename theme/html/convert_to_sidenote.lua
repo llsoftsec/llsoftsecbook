c7e0a57b246aedc08e574c64c745a03abdbc0087
Kristof Beyls <kristof.beyls@arm.com>
2023-01-24 14:01:57 +0100

    [HTML] Add unique ids to each sidenote
    Also make them toggleable by clicking on the id in the main text.
    Inline side notes are by default not visible.
    Side notes on the side are by default visible.

--- theme/html/convert_to_sidenote.lua
+++ theme/html/convert_to_sidenote.lua
@@ -1,14 +1,28 @@
 -- This lua pandoc filter converts footnotes to HTML "sidenote"s
 -- local logging = require 'theme/html/logging'
 
+sidenote_counter = 0;
+
 function Note (note)
   -- logging.temp('note', note)
-  -- Convert an Pandoc Node AST note to a <span class="sidenote"></span>
-  -- AST note.
-  -- A Note AST node has a "block" as content. A Span AST node needs an
-  -- "Inline" as content.
-  local span = pandoc.Span(pandoc.utils.blocks_to_inlines(note.content))
-  span.classes = {'sidenote'}
+  -- Convert a Pandoc Note AST node into AST nodes resembling:
+  --   <span class="sidenote_ref" href="#sidenote_xxx"><sup>ctr</sup></span>
+  --   <span id="sidenote_xxx" class="sidenote">ctr. xxx</span>
+  sidenote_counter = sidenote_counter + 1;
+  note_id = "sidenote_" .. sidenote_counter;
+  sidenote_content = {
+    pandoc.Str(sidenote_counter ..  ". "),
+    -- A Note AST node has a "block" as content. A Span AST node needs an
+    -- "Inline" as content.
+    table.unpack(pandoc.utils.blocks_to_inlines(note.content))};
+  local sidenote_span =
+    pandoc.Span(sidenote_content,
+                pandoc.Attr(note_id, {"sidenote"}, {}));
+  local sidenote_ref = pandoc.Span(
+    pandoc.RawInline(
+      "html",
+      "<sup>" .. sidenote_counter .. "</sup>"),
+      pandoc.Attr("", {"sidenote_ref"}, {href = "#" .. note_id}));
   -- logging.temp('span', span)
-  return span
+  return {sidenote_ref, sidenote_span}
 end
