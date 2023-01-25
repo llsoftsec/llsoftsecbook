4edb665f8801b36533e608b5eb157f246b0fba38
Kristof Beyls <kristof.beyls@arm.com>
2023-01-25 20:41:56 +0100

    [HTML] Show \todo and \missingcontent as sidenote.
    This shows todo's and missing contents as side notes in the HTML output.
    
    For small screens, where sidenotes are shown inline, this commit does
    not show the todos or missingcontents at all. It could be chosen later
    to have an indication (small button?) that the reader could click to
    make todos/missingcontent info visible.
    
    We could also implement a button in the top row to toggle visibility of
    todos/missingcontent.

--- theme/html/convert_to_sidenote.lua
+++ theme/html/convert_to_sidenote.lua
@@ -26,3 +26,76 @@ function Note (note)
   -- logging.temp('span', span)
   return {sidenote_ref, sidenote_span}
 end
+
+-- convert \todo's and \missingcontent's to HTML sidenotes
+-- They can appear as both RawBlock or RawInlines.
+
+todo_counter = 0;
+missingcontent_counter = 0;
+
+function get_raw_text(raw_content)
+  -- logging.temp('seeing rawblock/rawinline', raw_content);
+  if string.find(raw_content, "^\\todo{") then
+    todo_counter = todo_counter + 1;
+    note_id = "todo_" .. todo_counter;
+    keyword_length = string.len("\\todo{");
+    sidenote_class = "todo"
+    assert(string.find(raw_content, "}$"),
+           "\\todo doesn't end with '}'");
+  else if string.find(raw_content, "^\\missingcontent{") then
+      missingcontent_counter = missingcontent_counter + 1;
+      note_id = "missingcontent_" .. missingcontent_counter;
+      keyword_length = string.len("\\missingcontent{");
+      sidenote_class = "missingcontent"
+      assert(string.find(raw_content, "}$"),
+             "\\missingcontent doesn't end with '}'");
+    else
+      return nil;
+    end
+  end
+  local len = string.len(raw_content);
+  -- strip of '\\todo{' and '}'
+  local content = string.sub(raw_content, keyword_length+1, len-1);
+  -- logging.temp('recognized ' .. sidenote_class .. ': ' .. content);
+  return sidenote_class, note_id, content;
+end
+
+function create_spans(sidenote_class, note_id, content, isblock)
+  local sidenote_span =
+    pandoc.Span(pandoc.Str(content),
+                pandoc.Attr(note_id, {sidenote_class}, {}));
+  local sidenote_ref =
+    pandoc.Span(
+      pandoc.RawInline("html", "<sup>" .. sidenote_class .. "</sup>"),
+      pandoc.Attr("", {sidenote_class .. "_ref"}, {href = "#" .. note_id}));
+  if isblock then
+    sidenote_span = pandoc.Plain(sidenote_span);
+    sidenote_ref = pandoc.Plain(sidenote_ref);
+  end
+  return {sidenote_ref, sidenote_span}
+end
+
+
+function RawInline (rawinline)
+  if not rawinline.format == 'tex' then
+    return rawinline;
+  end
+  sidenote_class, note_id, rawcontent = get_raw_text(rawinline.text);
+  if sidenote_class == nil then
+    return rawblock;
+  end
+  return create_spans(sidenote_class, note_id, rawcontent, false);
+end
+
+function RawBlock (rawblock)
+  -- convert \todo's to HTML sidenotes.
+  -- \todo's are present as rawblocks for format tex.
+  if not rawblock.format == 'tex' then
+    return rawblock;
+  end
+  sidenote_class, note_id, content = get_raw_text(rawblock.text);
+  if sidenote_class == nil then
+    return rawblock;
+  end
+  return create_spans(sidenote_class, note_id, content, true);
+end
